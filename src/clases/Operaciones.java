7ee669f007ded3a8be6227f955d6d39cebb202f8
unknown <fgamboa91@hotmail.com>
2018-03-19 12:50:03 -0600

    Commit inicial

---
+++ src/clases/Operaciones.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author FGR
+ */
+public class Operaciones {
+    
+    public static int Sumar(int a, int b){
+        return a+b;
+    }
+    
+    public static double Sumar(double a, double b){
+        return a+b;
+    }
+    
+}
---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import clases.Operaciones;
+
+/**
+ *
+ * @author FGR
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Operaciones.Sumar(1, 2));
+        System.out.println(Operaciones.Sumar(4.2, 0.3));
+    }
+    
+}
