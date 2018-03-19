f9e297e00a3cc0b7d0ae9fa3e848c95273cc6720
unknown <fgamboa91@hotmail.com>
2018-03-19 13:04:46 -0600

    Commit 1.1

---
+++ src/clases/Operaciones.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author FGR
+ */
+public class Operaciones {
+    public static int Multiplicar(int a, int b){
+        return a*b;
+    }
+    public static double Multiplicar(double a, double b){
+        return a*b;
+    }
+}
